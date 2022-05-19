0e518ee5f831186aad4ebd72845869afb564ede4
Nafil Arzzam <92790024+Arzzam@users.noreply.github.com>
2022-05-19 21:52:20 +0530

    Roll button

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+function roll {
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
@@ -15,4 +16,5 @@ if (randomNumber1 < randomNumber2) {
     document.querySelector("h1").innerText = "Player 1 Won";
 } else {
     document.querySelector("h1").innerText = "Draw";
+}
 }
